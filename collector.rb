d0e4c0c762e8fc016962cb95e57f3c20d179285d
airled <airled@mail.ru>
2015-11-03 22:06:06 +0300

    message

--- collector.rb
+++ collector.rb
@@ -2,18 +2,22 @@ require 'nokogiri'
 require 'curb'
 require_relative './init/models'
 
-source = 'http://www.ticketpro.by'
-current_page = source + '/jnp/music/index.html'
+class Collector
 
-while (current_page != source) do
-  html = Nokogiri::HTML(Curl.get(current_page).body)
-  p current_page
-  html.xpath('//div[@class="eventInfo"]').map do |event|
-    name = event.xpath('.//h3').text.strip
-    place = event.xpath('.//p[1]').text.strip
-    date = event.xpath('.//p[2]').text.strip
-    pic_url = source + event.xpath('../div[@class="eventImage"]/div[@class="eventPoster"]/a/img/@src').text.strip
-    Concert.create(name: name, place: place, date: date, pic_url: pic_url)
+  def self.run
+    current_page = source + '/jnp/music/index.html'
+    while (current_page != source) do
+      html = Nokogiri::HTML(Curl.get(current_page).body)
+      p current_page
+      html.xpath('//div[@class="eventInfo"]').map do |event|
+        name = event.xpath('.//h3').text.strip
+        place = event.xpath('.//p[1]').text.strip
+        date = event.xpath('.//p[2]').text.strip
+        pic_url = source + event.xpath('../div[@class="eventImage"]/div[@class="eventPoster"]/a/img/@src').text.strip
+        Concert.create(name: name, place: place, date: date, pic_url: pic_url)
+      end
+      current_page = source + html.xpath('//a[@class="normal"]/@href').text
+    end
   end
-  current_page = source + html.xpath('//a[@class="normal"]/@href').text
+
 end
---
+++ message.rb
@@ -0,0 +1,48 @@
+require 'curb'
+require 'nokogiri'
+
+class Message
+
+  attr_accessor :text, :number
+
+  def send
+    html = get_html('http://freesms.mts.by/cgi-bin/cgi.exe?function=sms_send&isFree=1').text
+    # captcha_url = "http://freesms.mts.by/cgi-bin/cgi.exe?function=sms_show_antispam_image&ImageNumber=#{captcha_num}"
+    count = @text.size
+    time = Time.now
+    day = time.day.to_s
+    mon = time.mon.to_s
+    year = time.year.to_s
+    hour = time.hour.to_s
+    min = time.min
+    table_record_id = html.xpath('//td[@class="number"]/input[@type="hidden"]/@value').text
+
+    Curl.post('http://freesms.mts.by/cgi-bin/cgi.exe?function=sms_send', 
+      {
+        'MMObjectType' => '0',
+        'isFree' => '1',
+        'MMObjectID' => '',
+        'Prefix' => '375',
+        'To' => '37529' + @number,
+        'NDC' => '29',
+        'mobnum' => @number,
+        'Msg' => @text,
+        'count' => count,
+        'Day' => day,
+        'Mon' => mon,
+        'Year' => year,
+        'Hour' => hour,
+        'Min' => min,
+        'antispamText' => '3180', #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        'textTableRecordId' => table_record_id
+      })
+  end #send
+
+  private
+
+  def get_html(url)
+    Nokogiri::HTML(Curl.get(url).body)
+  end
+
+
+end #class
